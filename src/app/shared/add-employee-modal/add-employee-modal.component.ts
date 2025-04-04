c1a8af7b49080316efdf64be4d371fef4e673610
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2025-04-03 23:56:47 -0400

    Changed Profile Image Url To Radio Buttons
    Before users could entire a url for profile image. Now it is a set selection of images using radio buttons instead. Users have 8 profile images from bootdey to choose from. Also considering making custom profile images.

--- src/app/shared/add-employee-modal/add-employee-modal.component.ts
+++ src/app/shared/add-employee-modal/add-employee-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { EmployeeService } from '../../employee.service';
@@ -18,9 +18,21 @@ export class AddEmployeeModalComponent {
     email: '',
     phone: '',
     jobTitle: '',
-    imageUrl: '',
+    imageUrl: 'https://bootdey.com/img/Content/avatar/avatar1.png', // Default selection
     employeeCode: ''
   };
+
+  // Avatar options 1-8
+  avatarOptions = [
+    { id: 1, url: 'https://bootdey.com/img/Content/avatar/avatar1.png' },
+    { id: 2, url: 'https://bootdey.com/img/Content/avatar/avatar2.png' },
+    { id: 3, url: 'https://bootdey.com/img/Content/avatar/avatar3.png' },
+    { id: 4, url: 'https://bootdey.com/img/Content/avatar/avatar4.png' },
+    { id: 5, url: 'https://bootdey.com/img/Content/avatar/avatar5.png' },
+    { id: 6, url: 'https://bootdey.com/img/Content/avatar/avatar6.png' },
+    { id: 7, url: 'https://bootdey.com/img/Content/avatar/avatar7.png' },
+    { id: 8, url: 'https://bootdey.com/img/Content/avatar/avatar8.png' }
+  ];
 
   constructor(
     public activeModal: NgbActiveModal,
--- src/app/shared/edit-employee-modal/edit-employee-modal.component.ts
+++ src/app/shared/edit-employee-modal/edit-employee-modal.component.ts
@@ -8,10 +8,23 @@ import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './edit-employee-modal.component.html'
+  templateUrl: './edit-employee-modal.component.html',
+  styleUrls: ['./edit-employee-modal.component.css']
 })
 export class EditEmployeeModalComponent {
   @Input() employee!: Employee;
+
+  // Avatar options 1-8
+  avatarOptions = [
+    { id: 1, url: 'https://bootdey.com/img/Content/avatar/avatar1.png' },
+    { id: 2, url: 'https://bootdey.com/img/Content/avatar/avatar2.png' },
+    { id: 3, url: 'https://bootdey.com/img/Content/avatar/avatar3.png' },
+    { id: 4, url: 'https://bootdey.com/img/Content/avatar/avatar4.png' },
+    { id: 5, url: 'https://bootdey.com/img/Content/avatar/avatar5.png' },
+    { id: 6, url: 'https://bootdey.com/img/Content/avatar/avatar6.png' },
+    { id: 7, url: 'https://bootdey.com/img/Content/avatar/avatar7.png' },
+    { id: 8, url: 'https://bootdey.com/img/Content/avatar/avatar8.png' }
+  ];
 
   constructor(
     public activeModal: NgbActiveModal,
