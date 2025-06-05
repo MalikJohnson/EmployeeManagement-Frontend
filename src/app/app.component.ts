bbf71545f2586739aa64cbf6ac702e065f4d6870
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2025-06-05 10:08:23 -0400

    Fixed dropdown nav not working on mobile
    On mobile dropdown nav wasn't working. This was due to using bootstrap instead of ng bootstrap. I changed to ng bootstrap later in the project and didn't change this section causing a bug.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './auth.service';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive, NgbDropdownModule],
+  imports: [
+    RouterOutlet, 
+    CommonModule, 
+    RouterLink, 
+    RouterLinkActive, 
+    NgbDropdownModule,
+    NgbCollapseModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isCollapsed = true;
+  
   constructor(public authService: AuthService) {}
 }
